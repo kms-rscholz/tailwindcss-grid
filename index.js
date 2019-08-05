d328f42b507ce855c8044eb15c704dcc4a90956a
Robin Scholz <r.scholz@kms-team.com>
2019-08-05 10:29:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ module.exports = function ({
           },
         })),
         ...grids.map(rows => ({
-          [`.grid-rows-${columns}`]: {
+          [`.grid-rows-${rows}`]: {
             gridTemplateRows: `repeat(${rows}, 1fr)`,
           },
         })),
